4e28e677f7d683ff565234a4dccb78cef698501f
Layanne Cantanhede <layrodrgs@gmail.com>
2020-10-12 16:59:22 -0300

    Bean Validation e Exception Handler

--- src/main/java/com/estudos/libraryapi/api/dto/BookDTO.java
+++ src/main/java/com/estudos/libraryapi/api/dto/BookDTO.java
@@ -2,14 +2,23 @@ package com.estudos.libraryapi.api.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+
+
 @Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class BookDTO {
 
     private Long id;
+
+    @NotEmpty
     private String title;
+
+    @NotEmpty
     private String author;
+
+    @NotEmpty
     private String isbn;
 }
--- src/main/java/com/estudos/libraryapi/api/resources/BookController.java
+++ src/main/java/com/estudos/libraryapi/api/resources/BookController.java
@@ -1,11 +1,18 @@
 package com.estudos.libraryapi.api.resources;
 
 import com.estudos.libraryapi.api.dto.BookDTO;
+import com.estudos.libraryapi.api.exceptions.ApiErrors;
 import com.estudos.libraryapi.model.entity.Book;
 import com.estudos.libraryapi.service.BookService;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -22,10 +29,18 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO create( @RequestBody BookDTO dto){
+    public BookDTO create( @RequestBody @Valid BookDTO dto){
         Book entity = modelMapper.map(dto, Book.class);
         entity = service.save(entity);
 
         return modelMapper.map(entity, BookDTO.class);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handlerValidationException(MethodArgumentNotValidException ex){
+        BindingResult bindingResult = ex.getBindingResult();
+        return new ApiErrors(bindingResult);
+
     }
 }
