7e30efb2ec00a624e7d4eb76da9c0182792b8bc9
layrodrgs <layrodrgs@gmail.com>
2020-10-13 09:49:51 -0300

    Corrigindo dependencia de validacao

--- src/main/java/com/estudos/libraryapi/model/entity/Book.java
+++ src/main/java/com/estudos/libraryapi/model/entity/Book.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Book {
+
     private Long id;
     private String title;
     private String author;
---
+++ src/test/java/com/estudos/libraryapi/resources/BookControllerTest.java
@@ -0,0 +1,84 @@
+package com.estudos.libraryapi.resources;
+
+import com.estudos.libraryapi.api.dto.BookDTO;
+import com.estudos.libraryapi.model.entity.Book;
+import com.estudos.libraryapi.service.BookService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+    static String BOOK_API = "/api/books";
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    BookService service;
+
+    @Test
+    @DisplayName("Deve criar um livro com sucesso.")
+    public void createBookTest() throws Exception{
+
+        BookDTO dto = BookDTO.builder().author("Layanne").title("As aventuras").isbn("001").build();
+        Book savedBook = Book.builder().id((long) 10).author("Layanne").title("As aventuras").isbn("001").build();
+
+        BDDMockito.given(service.save(Mockito.any(Book.class)))
+                .willReturn(savedBook);
+        String json = new ObjectMapper().writeValueAsString(dto);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc
+                .perform(request)
+                .andExpect(status().isCreated() )
+                .andExpect(jsonPath("id").isNotEmpty())
+                .andExpect(jsonPath("title").value(dto.getTitle()))
+                .andExpect(jsonPath("author").value(dto.getAuthor()))
+                .andExpect(jsonPath("isbn").value(dto.getIsbn()));
+    }
+
+
+    @Test
+    @DisplayName("Deve lançar erro de validação quando não houver dados suficientes para criação do livro.")
+    public void createInvalidBookTest() throws Exception{
+
+        String json = new ObjectMapper().writeValueAsString(new BookDTO());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc.perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("errors", hasSize(3)));
+    }
+}
